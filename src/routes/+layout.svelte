692449faebfc01739cf787cdc6d7683492f1dee7
Justin Ahinon <justiny.ahinon@gmail.com>
2023-09-02 09:42:40 +0200

    misc

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,22 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+
+	export let data;
 </script>
 
 <nav>
 	<a href="/">Home</a>
-	<a href="/profile">Profile</a>
-	<a href="/auth/signup">Sign Up</a>
-	<a href="/auth/login">Login</a>
-	<form method="post" action="/profile?/logout" use:enhance>
-		<button type="submit">Log Out</button>
-	</form>
+
+	{#if data.user}
+		<a href="/profile">Profile</a>
+
+		<form method="post" action="/profile?/logout" use:enhance>
+			<button type="submit">Log Out</button>
+		</form>
+	{:else}
+		<a href="/auth/signup">Sign Up</a>
+		<a href="/auth/login">Login</a>
+	{/if}
 </nav>
 
 <slot />
