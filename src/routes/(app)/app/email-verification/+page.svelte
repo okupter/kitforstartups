718db81b28f1daaf8e932b0032e8f7ccbee0b97b
Justin Ahinon <justiny.ahinon@gmail.com>
2023-09-06 12:15:42 +0200

    misc

--- src/routes/(app)/app/email-verification/+page.svelte
+++ src/routes/(app)/app/email-verification/+page.svelte
@@ -6,19 +6,23 @@
 	<title>Email Verification</title>
 </svelte:head>
 
-<div>
+<div class="space-y-8">
 	<h1>Email Verification</h1>
-	<p>
-		Your email verification link was sent to your inbox. Please check your email and click the link
-		to verify your email address.
-	</p>
+	<div class="space-y-2">
+		<p>
+			Your email verification link was sent to your inbox. Please check your email and click the
+			link to verify your email address.
+		</p>
 
-	<p>If you did not receive the email, please check your spam folder or try to resend the email.</p>
+		<p>
+			If you did not receive the email, please check your spam folder or try to resend the email.
+		</p>
+	</div>
 
-	<div>
+	<div class="space-y-4">
 		<h2>Resend verification link</h2>
 		<form method="post" action="?/resendEmailVerificationLink" use:enhance>
-			<button type="submit">Resend</button>
+			<button type="submit" class="action-base action-primary w-fit">Resend</button>
 		</form>
 	</div>
 </div>
