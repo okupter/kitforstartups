0b96bc9be7e44fcc64157300d2232197e2d92d70
Justin Ahinon <justiny.ahinon@gmail.com>
2023-09-07 04:13:19 +0200

    progressive enhancement for signup

--- src/routes/(auth)/auth/signup/+page.svelte
+++ src/routes/(auth)/auth/signup/+page.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import type { SubmitFunction } from './$types';
+
+	let running = false;
+
+	const submitSignupUser: SubmitFunction = () => {
+		running = true;
+
+		return async ({  update }) => {
+			running = false;
+			await update();
+		};
+	};
 </script>
 
 <svelte:head>
@@ -12,7 +24,7 @@
 </div>
 
 <div class="p-8 border border-gray-300 rounded-sm shadow-sm">
-	<form method="post" action="?/signupUser" use:enhance>
+	<form method="post" action="?/signupUser" use:enhance={submitSignupUser}>
 		<div class="form-control">
 			<label for="firstName">First Name</label>
 			<input type="text" name="firstName" placeholder="First Name" />
@@ -33,7 +45,23 @@
 			<input type="password" name="password" placeholder="Your password" required />
 		</div>
 
-		<button type="submit" class="action-base action-primary">Sign Up</button>
+		<button type="submit" disabled={running} class="action-base action-primary">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				class="lucide lucide-rotate-cw {running ? 'w-4 h-4 opacity-100 transition-all mr-2 animate-spin duration-500' : 'h-0 w-0 opacity-0 mr-0'}"
+				><path d="M21 12a9 9 0 1 1-9-9c2.52 0 4.93 1 6.74 2.74L21 8" /><path d="M21 3v5h-5" /></svg
+			>
+
+			<span>Sign Up</span>
+		</button>
 	</form>
 </div>
 
