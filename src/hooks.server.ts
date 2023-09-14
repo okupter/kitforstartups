ddd04c41f1c1e992aebd0aa9192a2a72d09d2dce
Justin Ahinon <justiny.ahinon@gmail.com>
2023-09-14 11:03:52 +0100

    Update hooks.server.ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,36 @@
 import { auth } from '$lib/lucia/mysql';
+import { redirect, type Handle } from '@sveltejs/kit';
+import { sequence } from '@sveltejs/kit/hooks';
 
-export const handle = async ({ event, resolve }) => {
+const protectedRoutesBase = '/app';
+const emailVerificationPath = '/app/email-verification';
+const authRoutesBase = '/auth';
+
+const authHandler: Handle = async ({ event, resolve }) => {
 	event.locals.auth = auth.handleRequest(event);
+	const session = await event.locals.auth.validate();
+
+	if (!session) {
+		// If the user is not logged in and is trying to access a protected route,
+		// redirect them to the login page
+		// Except if they are trying to access the email verification page
+		if (
+			event.url.pathname.startsWith(protectedRoutesBase) &&
+			!event.url.pathname.startsWith(emailVerificationPath)
+		) {
+			throw redirect(302, '/auth/login');
+		}
+	}
+
+	if (session) {
+		// If the user is logged in and is trying to access an auth route,
+		// redirect them to the profile page
+		if (event.url.pathname.startsWith(authRoutesBase)) {
+			throw redirect(302, '/app/profile');
+		}
+	}
 
 	return await resolve(event);
 };
+
+export const handle = sequence(authHandler);
