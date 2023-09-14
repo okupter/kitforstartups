8085f1c866940186aa5ddc8a90e6691849407133
Justin Ahinon <justiny.ahinon@gmail.com>
2023-09-14 12:28:15 +0100

    Add Toast component
    The component uses `melt-ui`

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import '$styles/app.css';
 	import '@fontsource-variable/inter';
+
+	import Toast from '$lib/components/Toast.svelte';
 </script>
+
+<Toast />
 
 <slot />
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,14 @@
+import { preprocessMeltUI } from '@melt-ui/pp';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import sequence from 'svelte-sequential-preprocessor';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [vitePreprocess({})],
+	preprocess: sequence([
+		vitePreprocess({}),
+		preprocessMeltUI()
+	]),
 
 	kit: {
 		adapter: adapter(),
