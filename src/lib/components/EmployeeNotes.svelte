fa19c47280204d5249336df805409d903323dcff
drewpayment <drew.payment@gmail.com>
2023-10-27 22:56:01 -0400

    🔧 chore(package.json): add dayjs library as a dependency to handle date formatting in EmployeeNotes component 🐛 fix(EmployeeNotes.svelte): format the 'created' date in the notes using dayjs library to display it in the format 'MM-DD-YYYY' 🎨 style(EmployeeNotes.svelte): adjust the layout and styling of the Add Note button and the notes list for better visual presentation

--- src/lib/components/EmployeeNotes.svelte
+++ src/lib/components/EmployeeNotes.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import type { SelectEmployeeNotes } from '$lib/types/db.model';
 	import { createLabel, melt, createToolbar, createSeparator } from '@melt-ui/svelte';
+  import dayjs from 'dayjs';
+	import SubmitButton from './SubmitButton.svelte';
   
   export let notes: SelectEmployeeNotes[];
 
@@ -14,6 +16,14 @@
     orientation: 'horizontal',
     decorative: true,
   });
+  
+  const formatDate = (date: bigint) => {
+    const datejs = dayjs(Number(date));
+    
+    if (datejs.isValid()) return datejs.format('MM-DD-YYYY');
+    
+    return '';
+  }
 </script>
 
 <form action="">
@@ -33,30 +43,27 @@
 			class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
 		></textarea>
     
-    <div use:melt={$horizontal} class="my-3.5 h-[1px] w-full bg-neutral-900" />
+    <div class="flex justify-end w-[100%]">
+      <div class="pt-1">
+        <SubmitButton text="Add Note" />
+      </div>
+    </div>
     
-    <div class="block w-full rounded-md border-1 py-1 5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset">
-      {#each notes as note}
-        <div class="flex flex-row items-center justify-between">
-          <div class="flex flex-col">
-            <span class="text-sm font-medium leading-6 text-gray-900">{note.created}</span>
+    <div use:melt={$horizontal} class="my-3.5 h-[1px] w-full bg-neutral-400" />
+    
+    <div class="block w-full rounded-md border-1 p-4 5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset max-h-60 overflow-y-scroll">
+      {#each notes as note, i}
+        <div class="grid grid-cols-10 items-center justify-between gap-1">
+          <div class="flex flex-col col-span-9">
             <span class="text-sm font-medium leading-6 text-gray-900">{note.note}</span>
           </div>
-          <div class="flex flex-row items-center justify-center">
-            <button type="button" class="p-1.5 text-gray-900 hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 8a6 6 0 00-6-6m6 0a6 6 0 01-6 6"/>
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"/>
-              </svg>
-            </button>
-            <button type="button" class="p-1.5 text-gray-900 hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"/>
-              </svg>
-            </button>
+          <div class="flex flex-col">
+            <span class="text-sm font-medium leading-6 text-gray-900">{formatDate(note.created)}</span>
           </div>
         </div>
+        {#if i !== notes.length - 1}
+          <div use:melt={$horizontal} class="my-3.5 h-[1px] w-full bg-neutral-200" />
+        {/if}
       {/each}
     </div>
 	</div>
