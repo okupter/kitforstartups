14f3066f8d19e0d23405141d2df63c903c3213f7
Justin Ahinon <justiny.ahinon@gmail.com>
2024-03-28 20:42:55 +0300

    Update send.ts

--- starter/src/lib/emails/send.ts
+++ starter/src/lib/emails/send.ts
@@ -3,11 +3,11 @@ import { sendTestEmail } from '$lib/emails/mailhog';
 import { sendResendEmail } from '$lib/emails/resend';
 
 const sendEmail = async (options: { from: string; to: string; subject: string; html: string }) => {
-	if (!dev) {
+	if (dev) {
+		return await sendTestEmail(options);
+	} else {
 		return await sendResendEmail(options);
 	}
-
-	return await sendTestEmail(options);
 };
 
 export { sendEmail };
