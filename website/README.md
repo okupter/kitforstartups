fa1e0b72a349b391b64abeb9f5d5ace241c25a13
Justin Ahinon <justiny.ahinon@gmail.com>
2024-06-07 19:48:12 +0300

    Add website

---
+++ website/README.md
@@ -0,0 +1,5 @@
+# KitForStartups Website
+
+Website for KitForStartups, the Open Source SvelteKit SaaS boilerplate.
+
+[https://kitforstartups.com](https://kitforstartups.com)
---
+++ website/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<title>KitForStartups</title>
+	<meta
+		name="description"
+		content="KitForStartups is a starter kit for building and shipping fast, secure, and scalable full stack SaaS applications with SvelteKit and TypeScript."
+	/>
+</svelte:head>
+
+<h1>KitForStartups</h1>
+
+<a href="https://docs.kitforstartups.com">Documentation</a>
+<a href="https://github.com/okupter/kitforstartups">GitHub</a>
